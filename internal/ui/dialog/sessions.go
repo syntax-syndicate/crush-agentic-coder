8661a1c4baa171f49b6f7ab3d4db7836cf5b5548
Drew Smirnoff <drew@charm.land>
2026-08-23 00:06:34 +0400

    fix(windows): scrolling up on modifier keys alone (#3598)
    Signed-off-by: drew <me@andrinoff.com>

--- internal/ui/dialog/sessions.go
+++ internal/ui/dialog/sessions.go
@@ -216,12 +216,15 @@ func (s *Session) HandleMsg(msg tea.Msg) Action {
 					return ActionSelectSession{sessionItem.Session}
 				}
 			default:
+				prevValue := s.input.Value()
 				var cmd tea.Cmd
 				s.input, cmd = s.input.Update(msg)
 				value := s.input.Value()
-				s.list.SetFilter(value)
-				s.list.ScrollToTop()
-				s.list.SetSelected(0)
+				if value != prevValue {
+					s.list.SetFilter(value)
+					s.list.ScrollToTop()
+					s.list.SetSelected(0)
+				}
 				return ActionCmd{cmd}
 			}
 		}
